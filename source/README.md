9ba86516afae3da80a4acc9b087c7ccd2a279086
Flavia Ibañez <flavia.ibanez@gmail.com>
2012-08-23 15:34:23 -0300

    subiendo archivos

---
+++ source/README.md
@@ -0,0 +1,156 @@
+﻿widget-emz
+==========
+
+Widget de Eventos de Mendoza
+
+[Descargar de aquí](https://github.com/Noviggo/widget-emz/zipball/master)
+
+Instrucciones de uso:
+---------------------
+
+* Colocar el siguiente segmento de código en el área donde se desea colocar el widget:
+
+```html
+  <div>
+    <!-- Script widget emz -->
+    <link rel="stylesheet" href="http://noviggo.github.com/widget-emz/source/styles/emz_styles.css">
+    <script  type="text/javascript">document.write('<script src="http://noviggo.github.com/widget-emz/source/scripts/emz_script.js"><\/script>')</script>
+    <script type="text/javascript">
+      EMZ.init({
+        reparticion: 'Deportes',
+        limite: '2'
+      });
+    </script>
+ 
+    <div id="emz_widget"></div>
+    <!-- FIN Script widget emz -->
+  </div>
+```
+
+Insturcciones de uso si descarga el proyecto:
+----------------------------------------------------
+
+* Descargar el proyecto y colocarlo en una carpeta con acceso desde la web, por ejemplo __/widget-emz__
+* Colocar el siguiente segmento de código en el área donde se desea colocar el widget:
+
+```html
+  <!-- Script widget emz -->
+  <link rel="stylesheet" href="/widget-emz/styles/emz_styles.css">
+  <script type="text/javascript">document.write('<script src="/widget-emz/scripts/emz_script.js"><\/script>')</script>
+  <script type="text/javascript">
+    EMZ.init({});
+  </script>
+ 
+  <div id="emz_widget"></div>
+  <!-- FIN Script widget emz -->
+```
+
+* Recordar cambiar la sección _/widget-emz_ de la ruta tanto del script como de css si es necesario
+
+Personalización:
+----------------
+
+__Reparticiones__
+
+Si se desea obtener los resultados de alguna repartición en particular, colocarlo como parámetro _reparticion_ en _init()_, por ejemplo para "Deportes":
+
+```html
+  <!-- Script widget emz -->
+  <link rel="stylesheet" href="/widget-emz/styles/emz_styles.css">
+  <script  type="text/javascript">document.write('<script src="/widget-emz/scripts/emz_script.js"><\/script>')</script>
+  <script type="text/javascript">
+    EMZ.init({
+      reparticion: 'Deportes'
+    });
+  </script>
+ 
+  <div id="emz_widget"></div>
+  <!-- FIN Script widget emz -->
+```
+
+Los valores permitidos son:
+
+* Deportes
+* Turismo
+* Cultura
+
+__Cantidad de eventos a mostrar__
+
+El sistema permite también personalizar la cantidad de eventos a traer, utilizando el argumento _limite_, por ejemplo, para traer los próximos 5 eventos de Turismo:
+
+```html
+  <!-- Script widget emz -->
+  <link rel="stylesheet" href="/widget-emz/styles/emz_styles.css">
+  <script type="text/javascript">document.write('<script src="/widget-emz/scripts/emz_script.js"><\/script>')</script>
+  <script type="text/javascript">
+    EMZ.init({
+      reparticion: 'Turismo',
+      limite: '5'
+    });
+  </script>
+ 
+  <div id="emz_widget"></div>
+  <!-- FIN Script widget emz -->
+```
+
+__Personalización de estilos y dimensiones__
+
+El elemento _#emz_widget_ puede ser estilado si se requiere, y es posible agregarle los nombres de clases que sean necesarios. Tenga en cuenta que los elementos del widget utilizan el prefijo emz_ para evitar conflictos con sus nombres actuales.
+
+Existen dos formas de visualizar el widget dependiendo si se añade el nombre de clase _emz_tiny_ o no. El resultado es un listado de enventos sin el copete o descripcion:
+
+<img src="http://noviggo.github.com/widget-emz/source/images/widget_tiny.jpg"/> o <img src="http://noviggo.github.com/widget-emz/source/images/widget_normal.jpg"/>
+
+El widget ha sido diseñado para adaptarse al ancho del contenedor en que se lo posicione, con un mínimo de 250px. El alto dependerå de la cantidad de eventos solicitados o puede asignarsele (o utilizar los ya generados) un alto fijo teniendo en cuenta los siguientes requisitos:
+
+* Debe estar dentro de un contenedor con alto y ancho asignado por css. Además, debe modificar el alto de la caja _emz_content_ de la siguiente manera:
+
+```css
+  .emz_content{
+	height:00px; /*asigne el valor de su conveniencia*/
+	overflow-x:hidden;
+  }
+```
+
+* Si se muestran mas elementos de los que entran en el tamaño especificado, se debe agregar a la caja contenedora la clase _emz_scroll_ para agregar la barra de desplazamiento.
+* Tamaños predefinidos. Si desea utilizar estos tamaños solo agregue el nombre de clase en el contenedor _( ejemplo: emz_386x300 = width:386px; height:300px)_
+ 
+ * emz_386x300
+ * emz_300x250
+ * emz_300x250
+ * emz_300x300
+ * emz_250x250
+ * emz_290x200
+ * emz_290x200
+ * emz_270x200
+ * emz_300x300
+ * emz_300x250
+ * emz_320x320
+ * emz_290x300
+ * emz_540x300
+
+Ejemplo de uso:
+---------------
+
+```html
+  <div class="emz_tiny emz_300x300">
+    <!-- Script widget emz -->
+    <link rel="stylesheet" href="http://noviggo.github.com/widget-emz/source/styles/emz_styles.css">
+    <script  type="text/javascript">document.write('<script src="http://noviggo.github.com/widget-emz/source/scripts/emz_script.js"><\/script>')</script>
+    <script type="text/javascript">
+      EMZ.init({
+        reparticion: 'Deportes',
+        limite: '2'
+      });
+    </script>
+ 
+    <div id="emz_widget"></div>
+    <!-- FIN Script widget emz -->
+  </div>
+```
+
+y se vera de la siguiente manera:
+
+<img src="http://noviggo.github.com/widget-emz/source/images/widget_tiny.jpg"/>
+
+* Cuando se utiliza un tamaño predefinido no es necesario utilizar la clase _emz_scroll_
---
+++ source/scripts/emz_script.js
@@ -0,0 +1,258 @@
+/* ========================================================
+ * emz_script.js v1.0.0 - Noviggo
+ * ========================================================
+ * Creado por Guillermo Nuñez
+ * Copyright 2012 Guillermo Nuñez All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ * 
+ *  1. Redistributions of source code must retain the above copyright notice, this list of
+ *     conditions and the following disclaimer.
+ * 
+ *  2. Redistributions in binary form must reproduce the above copyright notice, this list
+ *     of conditions and the following disclaimer in the documentation and/or other materials
+ *     provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY GUILLERMO NUÑEZ ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL GUILLERMO NUÑEZ OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * The views and conclusions contained in the software and documentation are those of the
+ * authors and should not be interpreted as representing official policies, either expressed
+ * or implied, of Guillermo Nuñez.
+ * ======================================================== */
+
+
+var EMZ = function () {
+  var
+    /* El sitio web */
+    baseurl = 'http://www.eventos.mendoza.gov.ar',
+    
+    /* La reparticion. Valores aceptados:
+     * '': Para todos
+     * 'Deportes'
+     * 'Cultura'
+     * 'Turismo'
+     */
+    reparticion = '',
+    classReparticion = '',
+    idWidget = 'emz_widget',
+    
+    /* Cantidad de eventos a traer */
+    limit = '3',
+    
+    /*url de búsqueda de la API */
+    apiurl = '/api/v1/evento/buscar/',
+    
+    /* El elemento donde se creará el widget */
+    emzWidget,
+    
+    /* Objeto encargado de JSONP */
+    aObj;
+
+  /*
+   * Constructor del sistema de Ajax para JSON
+   */
+  function JSONscriptRequest (fullUrl) {
+      this.fullUrl = fullUrl; 
+      this.noCacheIE = '&noCacheIE=' + (new Date()).getTime();
+      this.headLoc = document.getElementsByTagName("head").item(0);
+      this.scriptId = 'JscriptId' + JSONscriptRequest.scriptCounter++;
+  }
+
+  /*
+   * Static script ID counter
+   */
+  JSONscriptRequest.scriptCounter = 1;
+
+  /*
+   * buildScriptTag method
+   */
+  JSONscriptRequest.prototype.buildScriptTag = function () {
+
+      this.scriptObj = document.createElement("script");
+      
+      this.scriptObj.setAttribute("type", "text/javascript");
+      this.scriptObj.setAttribute("charset", "utf-8");
+      this.scriptObj.setAttribute("src", this.fullUrl + this.noCacheIE);
+      this.scriptObj.setAttribute("id", this.scriptId);
+  }
+   
+  /*
+   * removeScriptTag method
+   */
+  JSONscriptRequest.prototype.removeScriptTag = function () {
+      // Destroy the script tag
+      this.headLoc.removeChild(this.scriptObj);  
+  }
+
+  /*
+   * addScriptTag method
+   */
+  JSONscriptRequest.prototype.addScriptTag = function () {
+      // Create the script tag
+      this.headLoc.appendChild(this.scriptObj);
+  }
+  
+  /*
+   * Transforma mes en número a texto de 3 letras
+   */
+  function mes(n){
+
+      meses = ["Ene", "Feb", "Mar", "Abr",
+              "May", "Jun", "Jul", "Ago",
+              "Sep", "Oct", "Nov", "Dic"];
+
+      if(n.charAt(0) == "0"){
+          n = parseInt(n.charAt(1), 10);
+      }else{
+          n = parseInt(n, 10);
+      }
+
+      return meses[n - 1];
+  }
+
+    /*
+     * Para crear fragmentos html para insertar
+     */
+    function create(htmlStr) {
+        var frag = document.createDocumentFragment(),
+            temp = document.createElement('div');
+        temp.innerHTML = htmlStr;
+        while (temp.firstChild) {
+            frag.appendChild(temp.firstChild);
+        }
+        return frag;
+    }
+  
+  /*
+   * Función de callback para recibir los datos de JSON
+   */
+  function callbackfunc (jsonData) {
+
+    var strWidget="";
+    strWidget += "<div class=\"emz_wrapper " + classReparticion + "\">";
+    strWidget += "  <div class=\"emz_header\">";
+    strWidget += "    <span class=\"emz_header_title\">Agenda de Eventos<\/span>";
+    strWidget += "  <\/div>";
+    strWidget += "  <div class=\"emz_content\">";
+
+    for(var i = 0; i < jsonData.objects.length; i++) {
+      var evento = jsonData.objects[i];
+      strWidget += "    <div class=\"emz_event\">";
+      strWidget += "      <div class=\"emz_title\"><a href=\"" + baseurl + '/evento/' + evento.id + '/' + evento.slug + "\">" + evento.nombre + "<\/a><\/div>";
+      strWidget += "      <div class=\"emz_date\">";
+      
+      fecha_hora = new Date(evento.inicio);
+      mes_fecha = fecha_hora.getMonth() + 1;
+      
+      strWidget += "        <span class=\"emz_day\">" + fecha_hora.getDate() + "<\/span>";
+      strWidget += "        <span class=\"emz_month\">" + mes(mes_fecha.toString()) + "<\/span>";
+      strWidget += "      <\/div>";
+      strWidget += "      <div class=\"emz_event_desc\">";
+      strWidget += "        <p>" + evento.copete + "<\/p>";
+      strWidget += "      <\/div>";
+      strWidget += "      <div class=\"emz_event_actions\">";
+      strWidget += "        <a href=\"" + baseurl + '/evento/' + evento.id + '/' + evento.slug + "\" class=\"emz_moreinfo\">Ver info completa +<\/a>";
+      strWidget += "      <\/div>";
+      strWidget += "    <\/div>";
+    }
+    strWidget += "    <div class=\"emz_buttons\">";
+    strWidget += "      <a href=\""+ baseurl + "\" class=\"emz_moreinfo\">Ver m&aacute;s eventos<\/a>";
+    strWidget += "    <\/div>";
+    strWidget += "  <\/div>";
+    strWidget += "  <div class=\"emz_footer\">";
+    strWidget += "    <div class=\"emz_brand " + classReparticion + "\"><\/div>";
+    strWidget += "  <\/div>";
+    strWidget += "<\/div>";
+
+    var fragment = create(strWidget);
+    emzWidget.replaceChild(fragment, emzWidget.childNodes[0]);
+
+    aObj.removeScriptTag();
+  }
+
+  function start() {
+    var url = baseurl + apiurl + "?q=" + reparticion + "&limit=" + limit + "&format=jsonp&callback=EMZ.callbackfunc";
+    emzWidget = document.getElementById(idWidget);
+
+    /* Crear mensaje de "Cargando" */
+    var strWidget="";
+    strWidget += "<div class=\"emz_wrapper " + classReparticion + "\">";
+    strWidget += "  <div class=\"emz_header\">";
+    strWidget += "    <span class=\"emz_header_title\">Agenda de Eventos<\/span>";
+    strWidget += "  <\/div>";
+    strWidget += "  <div class=\"emz_content\">";
+
+    strWidget += "  Cargando eventos ...";
+
+    strWidget += "    <div class=\"emz_buttons\">";
+    strWidget += "      <a href=\""+ baseurl + "\" class=\"emz_moreinfo\">Ver m&aacute;s eventos<\/a>";
+    strWidget += "    <\/div>";
+    strWidget += "  <\/div>";
+    strWidget += "  <div class=\"emz_footer\">";
+    strWidget += "    <div class=\"emz_brand " + classReparticion + "\"><\/div>";
+    strWidget += "  <\/div>";
+    strWidget += "<\/div>";
+
+    
+    var fragment = create(strWidget);
+    emzWidget.appendChild(fragment);
+    
+    aObj = new JSONscriptRequest(url);
+    aObj.buildScriptTag();
+    aObj.addScriptTag();
+    
+  }
+  
+  function init (args) {
+    if (args.reparticion) {
+        reparticion = args.reparticion;
+    }
+    if (args.idWidget) {
+        idWidget = args.idWidget;
+    }
+    if (args.limite) {
+        limit = args.limite;
+    }
+   
+    switch (reparticion) {
+        case 'Deportes':
+            classReparticion = 'emz_deportes';
+            break;
+        case 'Cultura':
+            classReparticion = 'emz_cultura';
+            break;
+        case 'Turismo':
+            classReparticion = 'emz_turismo';
+            break;
+        default: 
+            classReparticion = '';
+            break;
+    }
+    
+    
+    (function(i) {var u =navigator.userAgent;var e=/*@cc_on!@*/false; var st = 
+    setTimeout;if(/webkit/i.test(u)){st(function(){var dr=document.readyState;
+    if(dr=="loaded"||dr=="complete"){i()}else{st(arguments.callee,10);}},10);}
+    else if((/mozilla/i.test(u)&&!/(compati)/.test(u)) || (/opera/i.test(u))){
+    document.addEventListener("DOMContentLoaded",i,false); } else if(e){     (
+    function(){var t=document.createElement('doc:rdy');try{t.doScroll('left');
+    i();t=null;}catch(e){st(arguments.callee,0);}})();}else{window.onload=i;}})(start);
+
+  }
+  return {
+    init:init,
+    start:start,
+    callbackfunc:callbackfunc
+  }
+}();
+
+
